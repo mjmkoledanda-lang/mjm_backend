262441da8452325046feb7051fbf79cf60746f61
Mohamed Shazeen <62658770+Shazeen98@users.noreply.github.com>
2026-03-01 17:37:52 +0530

    Initial project sp

--- server.js
+++ server.js
@@ -15,8 +15,21 @@ const app = express();
 // ===============================
 // MIDDLEWARE
 // ===============================
+
+
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            "http://localhost:3000",
+            "https://mjmk.vercel.app"
+        ];
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
 }));
 
