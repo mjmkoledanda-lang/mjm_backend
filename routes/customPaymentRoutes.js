1368489e100ed05d32b4f08b6b2e388f5b84250c
Mohamed Shazeen <62658770+Shazeen98@users.noreply.github.com>
2026-03-05 00:48:32 +0530

    new payee11

--- routes/customPaymentRoutes.js
+++ routes/customPaymentRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const CustomPayment = require("../models/CustomPayment");
 
 
+// ==============================
 // ADD CUSTOM PAYMENT
+// ==============================
+
 router.post("/", async (req,res)=>{
 
     try{
@@ -16,27 +19,59 @@ router.post("/", async (req,res)=>{
 
     }catch(err){
 
-        res.status(500).json({message:"Failed to add payment"});
+        res.status(500).json({
+            message:"Failed to add payment"
+        });
 
     }
 
 });
 
 
+// ==============================
+// GET ALL CUSTOM PAYMENTS
+// ==============================
+
+router.get("/", async (req,res)=>{
+
+    try{
+
+        const payments = await CustomPayment
+            .find()
+            .sort({date:-1});
+
+        res.json(payments);
+
+    }catch(err){
+
+        res.status(500).json({
+            message:"Failed to fetch payments"
+        });
+
+    }
+
+});
+
+
+// ==============================
 // GET PAYMENTS BY FAMILY
+// ==============================
+
 router.get("/:familyId", async(req,res)=>{
 
     try{
 
         const payments = await CustomPayment.find({
-            familyId: req.params.familyId
+            familyId:req.params.familyId
         }).sort({date:-1});
 
         res.json(payments);
 
     }catch(err){
 
-        res.status(500).json({message:"Failed to fetch payments"});
+        res.status(500).json({
+            message:"Failed to fetch payments"
+        });
 
     }
 
