213f6b0f117008efc8850ccfb391e39101f8dfad
Mohamed Shazeen <62658770+Shazeen98@users.noreply.github.com>
2026-03-21 19:10:49 +0530

    notice1245

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(
         credentials: true,
     })
 );
-app.options("*", cors());
+app.options("/*", cors()); // ✅ correct
 // ===============================
 // 📦 BODY PARSERS
 // ===============================
