050db6a7ffc535b1b6fd548b1b8bd63048c2da68
mjmkoledanda-lang <mjm.koledanda@gmail.com>
2026-04-05 21:54:29 +0530

    fix manual search1

--- routes/familyRoutes.js
+++ routes/familyRoutes.js
@@ -27,7 +27,7 @@ router.post(
 router.get("/", protect, getAllFamilies);
 
 // ✅ SEARCH
-router.get("/search/:keyword", searchFamilies);cd
+router.get("/search/:keyword", searchFamilies);
 router.post("/filter", filterFamilies);
 const { getHeadStats } = require("../controllers/statsController");
 router.get("/stats/full", protect, getFullStats);
